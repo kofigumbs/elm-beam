83a2cb4b6a4f03eaa00285597fd51cbdfee1faad
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-04-19 18:31:45 -0500

    Write empty binary files in Main

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,24 @@
-import Pine.Compiler ()
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.ByteString.Lazy as LazyText
+import qualified Data.Map as Map
+import qualified System.Environment as Env
+
+import qualified AST.Module as Module
+import qualified Elm.Package as Package
+import qualified Pine.Compiler as Pine
+
 
 main :: IO ()
 main =
-  putStrLn "Hello, World!"
+  do path <- head <$> Env.getArgs
+     source <- readFile path
+
+     let (dealiaser, _, result) =
+           Pine.compile (Pine.Context Package.core False []) source mempty
+
+     case result of
+       Right (Pine.Result _ _ output) ->
+         LazyText.writeFile "pine.beam" output
+       Left errors ->
+         error $ concatMap (Pine.errorToString dealiaser path source) errors
--- src/Pine/Compiler.hs
+++ src/Pine/Compiler.hs
@@ -10,7 +10,7 @@ module Pine.Compiler
 
 import qualified Data.Aeson as Json
 import qualified Data.Map as Map
-import qualified Data.Text.Lazy as LazyText
+import qualified Data.ByteString.Lazy as LazyText
 import System.IO (Handle)
 
 import qualified AST.Module as Module
@@ -126,7 +126,7 @@ data Context = Context
 data Result = Result
     { _docs :: Maybe Docs.Documentation
     , _interface :: PublicModule.Interface
-    , _js :: LazyText.Text
+    , _js :: LazyText.ByteString
     }
 
 
