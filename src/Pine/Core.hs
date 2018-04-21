4124ea43da10383ad245d35d3228492d572ea252
Kofi Gumbs <h.kofigumbs@gmail.com>
2018-04-20 22:21:29 -0500

    Extrat module pieces

--- src/Pine/Core.hs
+++ src/Pine/Core.hs
@@ -21,16 +21,31 @@ interfaces =
     Module.Interface
       { Module.iVersion  = Compiler.version
       , Module.iPackage  = Package.core
-      , Module.iExports  = [ Variable.Union "Program" Variable.closedListing
-                           , Variable.Value "server"
-                           ]
+      , Module.iExports  = imports
       , Module.iImports  = mempty
-      , Module.iTypes    = Map.singleton "server" $
-                             Type.Lambda (Type.Var "a") $
-                                Type.App
-                                  (Type.Type (Variable.inCore ["Platform"] "Program"))
-                                  [Type.Var "flags", Type.Var "model", Type.Var "msg"]
-      , Module.iUnions   = Map.singleton "Program" (["flags", "model", "msg" ], [])
+      , Module.iTypes    = types
+      , Module.iUnions   = unions
       , Module.iAliases  = mempty
       , Module.iFixities = mempty
       }
+
+
+imports :: [Variable.Value]
+imports =
+  [ Variable.Union "Program" Variable.closedListing
+  , Variable.Value "server"
+  ]
+
+
+types :: Module.Types
+types =
+  Map.singleton "server" $
+    Type.Lambda (Type.Var "a") $
+       Type.App
+         (Type.Type (Variable.inCore ["Platform"] "Program"))
+         [Type.Var "flags", Type.Var "model", Type.Var "msg"]
+
+
+unions :: Module.Unions
+unions =
+  Map.singleton "Program" (["flags", "model", "msg" ], [])
